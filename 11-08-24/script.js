c7a6a9993df4369b453f22e609f1500ae7fc7d37
zakariyahrobiat <zakariyahrobiat2018@gmail.com>
2024-11-09 18:24:48 +0100

    commit

--- 11-08-24/script.js
+++ 11-08-24/script.js
@@ -4,23 +4,24 @@ const plan = document.querySelector(".plan")
 const phoneNumber= document.querySelector(".phone-number")
 const dob = document.querySelector(".date-of-birth")
 const initialEmptyRow = document.querySelector("tr > td[colspan='6']");
-
+count = 1
 function saveBtn(){
     if (initialEmptyRow) {
         initialEmptyRow.parentNode.remove();
     }
 const tr= document.createElement("tr")
 tr.innerHTML=`
-<td>1</td>
-<td> <div style="display:flex; flex-direction: column;  justify-content: center; align-items: start;">${fullName.value} <span style="background: #5B5B5B; color:white; font-weight: 400; border-radius: 5px;font-size: 10px;">${phoneNumber.value}</span></div></td>
-<td>${dob.value}</td>
-<td>${plan.value}</td>
-<td>${dob.value}</td>
+<td>${count}</td>
+<td><div class="table-container">${fullName.value} <span>${phoneNumber.value}</span></div></td>
+<td><div class="table-container">${dob.value} <span>${fullName.value}</div></td>
+<td><div class="table-container">${plan.value} <span>${dob.value}</div></td>
+<td><div class="table-container">${dob.value} <span>${fullName.value}</span></div></td>
 <td><div style="display:flex; justify-content: start; align-items: center; gap:2px;">
-<button style="background: #a12b2b; padding: 3px; width:auto; margin:0; font-weight: 400; border-radius: 5px;font-size: 10px;">More</button>
-<button style="background: #a12b2b; padding: 3px; width:auto; margin:0; font-weight: 400; border-radius: 5px;font-size: 10px;">Physio Note</>
+<button style="background: #a12b2b; padding: 3px 6px; width:auto; margin:0; font-weight: 400; border-radius: 5px;font-size: 10px;">More</button>
+<button style="background: #a12b2b; padding: 3px 6px; width:auto; margin:0; font-weight: 400; border-radius: 5px;font-size: 10px;">Physio Note</>
 </div>
 </td>
 `
 tBody.appendChild(tr)
+count++
 }
